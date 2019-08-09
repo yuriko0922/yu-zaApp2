29310a1ae6d341f91917bc9e5126c371f5d812d6
yuriko <yrk922@gmail.com>
2019-08-09 15:05:02 +0900

    firstcommit

---
+++ yu-zaApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yu-zaApp2
+//
+//  Created by 津國　由莉子 on 2019/08/09.
+//  Copyright © 2019 yurityann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
